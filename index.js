247c280c86cb543e663ae506ffaea0197cc5901a
Naoki <exparts_7@yahoo.co.jp>
2022-07-30 22:32:06 +0900

    Update 2022/07/27 23:30

--- index.js
+++ index.js
@@ -1,22 +1,103 @@
 
 // メニューにマウスオーバーするとサブメニューが開く
+
+// メニュー1
 const menu1 = document.getElementById('menu_main1');
 const submenu1 = document.getElementById('submenu_main1');
 
 menu1.addEventListener('mouseover', function() {
   submenu1.style.visibility = 'visible';
+  submenu1.style.opacity = 1;
 }, false);
 
 menu1.addEventListener('mouseleave', function() {
   submenu1.style.visibility = 'hidden';
+  submenu1.style.opacity = 0;
 }, false);
 
 submenu1.addEventListener('mouseover', function() {
   submenu1.style.visibility = 'visible';
+  submenu1.style.opacity = 1;
 }, false);
 
 submenu1.addEventListener('mouseleave', function() {
   submenu1.style.visibility = 'hidden';
+  submenu1.style.opacity = 0;
+}, false);
+
+
+// メニュー2
+const menu2 = document.getElementById('menu_main2');
+const submenu2 = document.getElementById('submenu_main2');
+
+menu2.addEventListener('mouseover', function() {
+  submenu2.style.visibility = 'visible';
+  submenu2.style.opacity = 1;
+}, false);
+
+menu2.addEventListener('mouseleave', function() {
+  submenu2.style.visibility = 'hidden';
+  submenu2.style.opacity = 0;
+}, false);
+
+submenu2.addEventListener('mouseover', function() {
+  submenu2.style.visibility = 'visible';
+  submenu2.style.opacity = 1;
+}, false);
+
+submenu2.addEventListener('mouseleave', function() {
+  submenu2.style.visibility = 'hidden';
+  submenu2.style.opacity = 0;
+}, false);
+
+
+// メニュー3
+const menu3 = document.getElementById('menu_main3');
+const submenu3 = document.getElementById('submenu_main3');
+
+menu3.addEventListener('mouseover', function() {
+  submenu3.style.visibility = 'visible';
+  submenu3.style.opacity = 1;
+}, false);
+
+menu3.addEventListener('mouseleave', function() {
+  submenu3.style.visibility = 'hidden';
+  submenu3.style.opacity = 0;
+}, false);
+
+submenu3.addEventListener('mouseover', function() {
+  submenu3.style.visibility = 'visible';
+  submenu3.style.opacity = 1;
+}, false);
+
+submenu3.addEventListener('mouseleave', function() {
+  submenu3.style.visibility = 'hidden';
+  submenu3.style.opacity = 0;
+}, false);
+
+
+// メニュー3
+const menu4 = document.getElementById('menu_main4');
+const submenu4 = document.getElementById('submenu_main4');
+
+menu4.addEventListener('mouseover', function() {
+  submenu4.style.visibility = 'visible';
+  submenu4.style.opacity = 1;
+}, false);
+
+menu4.addEventListener('mouseleave', function() {
+  submenu4.style.visibility = 'hidden';
+  submenu4.style.opacity = 0;
+}, false);
+
+submenu4.addEventListener('mouseover', function() {
+  submenu4.style.visibility = 'visible';
+  submenu4.style.opacity = 1;
+}, false);
+
+submenu4.addEventListener('mouseleave', function() {
+  submenu4.style.visibility = 'hidden';
+  submenu4.style.opacity = 0;
 }, false);
 
 
